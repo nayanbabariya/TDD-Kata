64b078589487307939d6dc78cbf1aa9c1cd7eaac
Nayan <nayan@auberginesolutions.com>
2025-01-26 13:29:10 +0530

    Code refactoring: Set 'actual' & 'matcher' in their proper order in all the tests

--- test/tdd_kata_test.dart
+++ test/tdd_kata_test.dart
@@ -3,27 +3,27 @@ import 'package:test/test.dart';
 
 void main() {
   test('Should return 0 when empty string is provided', () {
-    expect(0, add(''));
+    expect(add(''), 0);
   });
 
   test('Should return the input integer when a single number is provided', () {
-    expect(1, add('1'));
+    expect(add('1'), 1);
   });
 
   test('Should return the sum of 2 numbers when 2 comma separated numbers are provided', () {
-    expect(3, add('1,2'));
+    expect(add('1,2'), 3);
   });
 
   test('Should return the sum of all numbers when multiple comma separated numbers are provided', () {
-    expect(6, add('1,2,3'));
-    expect(10, add('1,2,3,4'));
+    expect(add('1,2,3'), 6);
+    expect(add('1,2,3,4'), 10);
   });
   
   test('Should return the sum of numbers when new line is used as separator instead of comma', () {
-    expect(6, add('1\n2,3'));
+    expect(add('1\n2,3'), 6);
   });
 
   test('Should return the sum of numbers when a custom delimiter is used. Delimiter is specified at the beginning of the input', () {
-    expect(3, add('//;\n1;2'));
+    expect(add('//;\n1;2'), 3);
   });
 }
