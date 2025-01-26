4e34881d9065387f003a45da0410552bc28bdd38
Nayan <nayan@auberginesolutions.com>
2025-01-26 11:22:08 +0530

    Add test for returning 0 when input is an empty string

--- test/tdd_kata_test.dart
+++ test/tdd_kata_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:tdd_kata/tdd_kata.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Should return 0 when empty string is provided', () {
+    expect(0, add(''));
+  });
+}
