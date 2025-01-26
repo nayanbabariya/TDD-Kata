5a558b6a340dd109b233cb8f2dcb8f4b1f1ca69b
Nayan <nayan@auberginesolutions.com>
2025-01-26 11:26:26 +0530

    Add test for returning the input integer when a single number is provided

--- test/tdd_kata_test.dart
+++ test/tdd_kata_test.dart
@@ -5,4 +5,8 @@ void main() {
   test('Should return 0 when empty string is provided', () {
     expect(0, add(''));
   });
+
+  test('Should return the input integer when a single number is provided', () {
+    expect(1, add('1'));
+  });
 }
