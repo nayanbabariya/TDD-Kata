7b0e6136477979d126265d00293c9406d446df71
Nayan <nayan@auberginesolutions.com>
2025-01-26 11:54:04 +0530

    Add test for returning the sum of 2 numbers when 2 comma separated numbers are provided

--- test/tdd_kata_test.dart
+++ test/tdd_kata_test.dart
@@ -9,4 +9,8 @@ void main() {
   test('Should return the input integer when a single number is provided', () {
     expect(1, add('1'));
   });
+
+  test('Should return the sum of 2 numbers when 2 comma separated numbers are provided', () {
+    expect(3, add('1,2'));
+  });
 }
